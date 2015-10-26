b99c6cce3a291e07413a3b8320ac67e269682e98
Paul Ryan <paul@sharpred.com>
2015-10-26 10:54:37 +0000

    added basic key

--- example/app/controllers/index.js
+++ example/app/controllers/index.js
@@ -1,3 +1,6 @@
-var woopra = require("com.stepupapps.tiwoopra");
-
+var Woopra = require("com.stepupapps.tiwoopra");
+var woopra = Woopra.create("performapp.stepupapps.com", {ssl: false});
+woopra.track('exampleApp', {
+    eventProperty: 'startup'
+});
 $.index.open();
